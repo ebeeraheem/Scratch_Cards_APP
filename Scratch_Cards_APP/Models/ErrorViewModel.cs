0d2e966f0de540b3304c11fadca252a13ca66a3c
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-24 14:24:14 +0100

    Add project files.

---
+++ Scratch_Cards_APP/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Scratch_Cards_APP.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
